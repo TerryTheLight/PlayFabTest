37626dcaa4e5d51fdb207926def162b1ff495fdc
Jun Jie <terrythelight1@gmail.com>
2017-03-10 10:15:07 +0800

    First Commit

---
+++ Assets/Scripts/PlayFab/LoginManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LoginManager : MonoBehaviour {
+
+	void OnEnable()
+    {
+        PlayFabManager.LoginCompleted += LoginComplete;
+        PlayFabManager.LoginFailed += LoginFailed;
+    }
+
+    void OnDisable()
+    {
+        PlayFabManager.LoginCompleted -= LoginComplete;
+        PlayFabManager.LoginFailed -= LoginFailed;
+    }
+
+    public void LoginAsGuest()
+    {
+        PlayFabManager.Instance.GuestLogin();
+    }
+
+    public void LoginWithFacebook()
+    {
+        FacebookManager.Instance.FBLogin();
+    }
+
+    void LoginComplete()
+    {
+        Debug.Log("Successfully login to PlayFab!");
+        SceneManager.LoadScene(1);
+    }
+
+    void LoginFailed()
+    {
+        Debug.LogAssertion("Login Failed...");
+    }
+}
